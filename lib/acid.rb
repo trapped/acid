7a45292a5c70bd2038d3dac0a33cd48909c76295
trapped <giorgio@pellero.it>
2015-03-10 22:20:36 +0100

    Refine logging, add some checks, clean some code

--- lib/acid.rb
+++ lib/acid.rb
@@ -3,36 +3,45 @@ require 'acid/worker'
 require 'yaml'
 require 'logger'
 
-LOG = LOG ||= Logger.new($stdout)
+LOG ||= Logger.new($stdout)
 
 module Acid
-  def self.start(id, dir, out = $stdout)
-    file = File.join(dir, 'acid.yml')
+  def self.start(id, dir, out = $stdout, cfg = ['acid.yml'])
+    file = ''
+    cfg.each do |name|
+      file = File.join dir, name
+      if File.exist?(file)
+        break
+      end
+    end
+    if file.length == 0
+      return -1
+    end
     config = Acid::Config.new
     config.read(file)
     # Run setup commands
     if config.setup.length > 0
       setup_worker = Acid::Worker.new(id, config.env, config.shell)
-      LOG.info('Acid#'+id) { "Executing setup for #{file}" }
+      LOG.info("Acid##{id}") { "Executing setup for #{file}" }
       config.setup.each { |command|
         result = setup_worker.run command, out
         # Command returns failure code, stop
         if result > 0
-          LOG.info('Acid#'+id) { "Command exited with #{result}, stopping" }
-          exit 1
+          LOG.info("Acid##{id}") { "Command exited with #{result}, stopping" }
+          return result
         end
       }
     end
     # Run exec commands
     if config.exec.length > 0
       exec_worker = Acid::Worker.new(id, config.env, config.shell)
-      LOG.info('Acid#'+id) { "Executing exec for file #{file}" }
+      LOG.info("Acid##{id}") { "Executing exec for file #{file}" }
       config.exec.each { |command|
         result = exec_worker.run command, out
         # Command returns failure code, stop
         if result > 0
-          LOG.info('Acid#'+id) { "Command exited with #{result}, stopping" }
-          exit 1
+          LOG.info("Acid##{id}") { "Command exited with #{result}, stopping" }
+          return result
         end
       }
     end
--- lib/acid/worker.rb
+++ lib/acid/worker.rb
@@ -2,53 +2,58 @@ require 'open3'
 require 'colorize'
 require 'logger'
 
-class Acid::Worker
-  # Creates a new Acid::Worker with the provided environment and shell
-  def initialize(id, env=[], shell)
-    @id = id
-    @env = env
-    @shell = shell
-  end
+module Acid
+  class Worker
+    # Creates a new Acid::Worker with the provided environment and shell
+    def initialize(id, env=[], shell)
+      @id = id
+      @env = env
+      @shell = shell
+    end
 
-  # Creates a thread that pipes streams until killed
-  def capture(stream_from, stream_to, &filter)
-    LOG.info('Acid::Worker#'+@id) { 'Starting capture thread' }
-    Thread.new {
-      while true # TODO: Using waitpid to check if the thread is alive kills it, but just looping like this is dangerous
-        r = stream_from.read
-        if filter # yield() syntax is less expensive performance-wise
-          r = filter.call(r)
+    # Creates a thread that pipes streams until killed
+    def capture(stream_from, stream_to, &filter)
+      LOG.info("Acid::Worker##{@id}") { "Starting capture thread (#{stream_from.inspect}->#{stream_to.inspect})" }
+      Thread.new {
+        while true # TODO: Using waitpid to check if the thread is alive kills it, but just looping like this is dangerous
+          r = stream_from.read
+          if filter # yield() syntax is less expensive performance-wise
+            r = filter.call(r)
+          end
+          stream_to.write r
         end
-        stream_to.write r
-      end
-    }
-  end
+      }
+    end
 
-  # Executes a single command
-  def run(command, output = $stdout)
-    LOG.info('Acid::Worker#'+@id) { "Running '#{command}'..." }
-    # Capture stdout and stderr separately http://ruby-doc.org/stdlib-2.1.4/libdoc/open3/rdoc/Open3.html#method-c-popen3
-    Open3.popen3(@env, [@shell, '"' + command.gsub("'", %q(\\\')).gsub('"', %q(\\\")) + '"'].join(' ')) { |stdin, stdout, stderr, wait_thr|
-      LOG.info('Acid::Worker#'+@id) { "Using #{@shell.split(' ')[0]}, PID is #{wait_thr.pid}" }
-      stdin.close # We don't need it and the process might wait for it to close if it needs input
-      # Loop while the child process is alive (nonblocking) http://stackoverflow.com/a/14381862/2386865
-      begin
-        # Print stdout and stderr to console
-        thr_out = self.capture(stdout, output)
-        thr_err = self.capture(stderr, output) { |text| text.red }
-        wait_thr.join
-        thr_out.kill; thr_err.kill
-      rescue Errno::ECHILD, Errno::EINVAL
-        # Process exited
-      end
-      val = wait_thr.value
-      if val != nil
-        LOG.info('Acid::Worker#'+@id) { "Exited with code #{val.exitstatus}" }
-        return val.exitstatus
-      else
-        LOG.error('Acid::Worker#'+@id) { 'Process vanished, exit code unavailable' }
-        return -1
-      end
-    }
+    # Executes a single command
+    def run(command, output = $stdout, dir = Dir.pwd)
+      LOG.info("Acid::Worker##{@id}") { "Running '#{command}' in '#{dir}'..." }
+      output.sync = true
+      # Capture stdout and stderr separately http://ruby-doc.org/stdlib-2.1.4/libdoc/open3/rdoc/Open3.html#method-c-popen3
+      Open3.popen3(@env, [@shell, '"' + command.gsub("'", %q(\\\')).gsub('"', %q(\\\")) + '"'].join(' '), chdir: dir) { |stdin, stdout, stderr, wait_thr|
+        stdout.sync = true
+        stderr.sync = true
+        LOG.info("Acid::Worker##{@id}") { "Using #{@shell.split(' ')[0]}, PID is #{wait_thr.pid}" }
+        stdin.close # We don't need it and the process might wait for it to close if it needs input
+        # Loop while the child process is alive (nonblocking) http://stackoverflow.com/a/14381862/2386865
+        begin
+          # Print stdout and stderr to console
+          thr_out = self.capture(stdout, output)
+          thr_err = self.capture(stderr, output) { |text| text.red }
+          wait_thr.join
+          thr_out.kill; thr_err.kill
+        rescue Errno::ECHILD, Errno::EINVAL
+          # Process exited
+        end
+        val = wait_thr.value
+        if val != nil
+          LOG.info("Acid::Worker##{@id}") { "Exited with code #{val.exitstatus}" }
+          return val.exitstatus
+        else
+          LOG.error("Acid::Worker##{@id}") { 'Process vanished, exit code unavailable' }
+          return -1
+        end
+      }
+    end
   end
 end
